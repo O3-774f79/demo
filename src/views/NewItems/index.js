458423ac5e597b874ed92fd3fa70e5f748254eb2
Moo-ananchai <ananchai.pha@gmail.com>
2019-01-25 04:25:01 +0700

    add step

--- src/views/NewItems/index.js
+++ src/views/NewItems/index.js
@@ -25,7 +25,7 @@ const emp = [
   { value: 'vanilla', label: 'ค' }
 ]
 const steps = [{
-  title: 'ฝ่ายจัดซื้อ',
+  title: 'ฝ่ายการตลาด',
   status: 'finish',
   icon: <Icon type="user" />
 }, {
@@ -33,25 +33,22 @@ const steps = [{
   status: 'process',
   icon: <Icon type="loading" />
 }, {
-  title: 'เสร็จสิ้น',
-  status: 'wait',
-  icon: <Icon type="smile-o" />
-}];
-const stepsBuyer = [{
-  title: 'ซุปเปอร์ไวเซอร์',
-  status: 'finish',
-  icon: <Icon type="user" />
-}, {
-  title: 'ประธานกลุ่มสินค้า',
-  status: 'process',
-  icon: <Icon type="loading" />
-}, {
-  title: 'กรรมการผู้จัดการฝ่าย',
+  title: 'ฝ่ายR&D',
   status: 'wait',
   icon: <Icon type="user" />
 },
 {
-  title: 'ผู้จัดการฝ่าย',
+  title: 'ฝ่ายวางแผนการผลิต',
+  status: 'wait',
+  icon: <Icon type="user" />
+}];
+const stepsBuyer = [{
+  title: 'Requested',
+  status: 'finish',
+  icon: <Icon type="user" />
+},
+{
+  title: 'Approver',
   status: 'wait',
   icon: <Icon type="user" />
 }];
@@ -112,9 +109,15 @@ export default class Index extends PureComponent {
           </Steps>
         </div>
         <Tabs onChange={callback} type="card">
+        <div style={{ padding: 10 }}>
+                    <Steps current={current}>
+                      {steps.map(item => <Step key={item.title} title={item.title} status={item.status} icon={item.icon} />)}
+                    </Steps>
+                  </div>
           <TabPane tab="ฝ่ายการตลาด" key="1">
             <Tabs defaultActiveKey="1">
               <TabPane tab={<span>ฝ่ายการตลาด</span>} key="1">
+   
                 <Card style={{ width: "100%", marginBottom: 100 }}>
                   <CardHeader>
                     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -126,11 +129,6 @@ export default class Index extends PureComponent {
                       </div>
                     </div>
                   </CardHeader>
-                  <div style={{ padding: 10 }}>
-                    <Steps current={current}>
-                      {steps.map(item => <Step key={item.title} title={item.title} status={item.status} icon={item.icon} />)}
-                    </Steps>
-                  </div>
                   <CardBody>
                     <Form>
                       <FormGroup row>
@@ -266,187 +264,6 @@ export default class Index extends PureComponent {
                 </Card>
               </TabPane>
             </Tabs>
-            <Card style={{ width: "100%", marginBottom: 100 }}>
-              <CardHeader>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <div>
-                    <strong><h4>ฝ่ายจัดซื้อ</h4></strong>
-                  </div>
-                  <div>
-                    <Button type="primary">ตกลง</Button>
-                  </div>
-                </div>
-              </CardHeader>
-              <div style={{ padding: 10 }}>
-                <Steps current={current}>
-                  {steps.map(item => <Step key={item.title} title={item.title} status={item.status} icon={item.icon} />)}
-                </Steps>
-              </div>
-              <CardBody>
-                <Form>
-                  <FormGroup row>
-                    <Label for="exampleEmail" sm={1}>Supplier Code</Label>
-                    <Col sm={4}>
-                      <Input type="email" name="email" id="exampleEmail" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="exampleEmail" sm={1}>ชื่อ Supplier</Label>
-                    <Col sm={4}>
-                      <Input type="email" name="email" id="exampleEmail" />
-                    </Col>
-                    <Label for="exampleEmail" sm={1} >เงื่อนไขการชำระเงิน</Label>
-                    <Col sm={1}>
-                      <Input type="email" name="email" id="exampleEmail" />
-                    </Col>
-                    <Label sm={1} >วัน</Label>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="SortName" sm={1}>Sort Name</Label>
-                    <Col sm={4}>
-                      <Input type="SortName" name="SortName" id="SortName" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="address" sm={1}>ที่อยู่</Label>
-                    <Col sm={11}>
-                      <Input type="textarea" name="SortName" id="address" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="contact" sm={1}>ผู้ติดต่อ</Label>
-                    <Col sm={4}>
-                      <Input type="email" name="email" id="contact" />
-                    </Col>
-                    <Label for="tell" sm={1} >เบอร์โทรศัพท์</Label>
-                    <Col sm={2}>
-                      <Input type="email" name="email" id="tell" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="email" sm={1}>Email</Label>
-                    <Col sm={4}>
-                      <Input type="email" name="email" id="email" />
-                    </Col>
-                    <Label for="fax" sm={1} >เบอร์แฟกซ์</Label>
-                    <Col sm={2}>
-                      <Input type="email" name="email" id="fax" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="email" sm={1}>ซุปเปอร์ไวเซอร์</Label>
-                    <Col sm={4}>
-                      <Select
-                        value={selectedOption1}
-                        onChange={this.handleChange1}
-                        placeholder={'กรุณาเลือกบริษัท'}
-                        options={options}
-                      />
-                    </Col>
-                    <Label for="fax" sm={1} >ซุปเปอร์ไวเซอร์</Label>
-                    <Col sm={2}>
-                      <Select
-                        value={selectedOption5}
-                        onChange={this.handleChange5}
-                        placeholder={'กรุณาเลือกซุปเปอร์ไวเซอร์'}
-                        options={emp}
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="email" sm={1}>ประธานกลุ่มสินค้า</Label>
-                    <Col sm={4}>
-                      <Select
-                        value={selectedOption2}
-                        onChange={this.handleChange2}
-                        placeholder={'กรุณาเลือกบริษัท'}
-                        options={options}
-                      />
-                    </Col>
-                    <Label for="fax" sm={1} >ประธานกลุ่มสินค้า</Label>
-                    <Col sm={2}>
-                      <Select
-                        value={selectedOption6}
-                        onChange={this.handleChange6}
-                        placeholder={'กรุณาเลือกประธานกลุ่มสินค้า'}
-                        options={emp}
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="email" sm={1}>ผู้จัดการฝ่าย</Label>
-                    <Col sm={4}>
-                      <Select
-                        value={selectedOption3}
-                        onChange={this.handleChange3}
-                        placeholder={'กรุณาเลือกบริษัท'}
-                        options={options}
-                      />
-                    </Col>
-                    <Label for="fax" sm={1} >ผู้จัดการฝ่าย</Label>
-                    <Col sm={2}>
-                      <Select
-                        value={selectedOption7}
-                        onChange={this.handleChange7}
-                        placeholder={'กรุณาเลือกผู้จัดการฝ่าย'}
-                        options={emp}
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="email" sm={1}>กรรมการผู้จัดการฝ่าย</Label>
-                    <Col sm={4}>
-                      <Select
-                        value={selectedOption4}
-                        onChange={this.handleChange4}
-                        placeholder={'กรุณาเลือกบริษัท'}
-                        options={options}
-                      />
-                    </Col>
-                    <Label for="fax" sm={1} >กรรมการผู้จัดการฝ่าย</Label>
-                    <Col sm={2}>
-                      <Select
-                        value={selectedOption8}
-                        onChange={this.handleChange8}
-                        placeholder={'กรุณาเลือกกรรมการผู้จัดการฝ่าย'}
-                        options={emp}
-                      />
-                    </Col>
-                  </FormGroup>
-                </Form>
-                <hr />
-                <div>รายละเอียด</div>
-                <br />
-
-                <hr />
-                <Checkbox.Group style={{ width: '100%' }} onChange={onChange}>
-                  <Row>
-                    <Col span={8}><Checkbox value="A">Supplier ที่ราคาถูกกว่า</Checkbox></Col>
-                    <Col span={8}><Checkbox value="B">Supplier ที่ราคาแพงกว่า แต่มีรายเดียว</Checkbox></Col>
-                    <Col span={8}><Checkbox value="C">Supplier รายใหมเ่พิ่ม Itemใหม่</Checkbox></Col>
-                  </Row>
-                  <Row>
-                    <Col span={8}><Checkbox value="D">Supplier ที่ราคาแพงกว่า และมีมากกว่า 1 ราย</Checkbox></Col>
-                    <Col span={8}><Checkbox value="E">Supplier รายใหม่เพิ่ม Itemใหม่ที่ฝ่ายอื่นหาให้ </Checkbox>
-                      <select>
-                        <option >ก</option>
-                        <option >ข</option>
-                        <option >ค</option>
-                        <option >ง</option>
-                      </select>
-                    </Col>
-                    <Col span={8}><Checkbox value="E">Supplier ที่ฝ่ายอื่นหาให้</Checkbox>
-                      <select>
-                        <option >ก</option>
-                        <option >ข</option>
-                        <option >ค</option>
-                        <option >ง</option>
-                      </select>
-                    </Col>
-                  </Row>
-                </Checkbox.Group>,
-                  </CardBody>
-            </Card>
           </TabPane>
           <TabPane tab="่ฝ่ายบัญชีและการเงิน" key="2">
             <Tabs defaultActiveKey="1">
@@ -1017,24 +834,7 @@ export default class Index extends PureComponent {
                   </CardBody>
             </Card>
     </TabPane>
-            </Tabs>
-           
-          </TabPane>
-
-          <TabPane tab="ฝ่ายวางแผนการผลิต" key="5">
-            <Card style={{ width: "100%" }}>
-              <CardHeader>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <div>
-                    <strong><h4>ฝ่ายบัญชีและการเงิน</h4></strong>
-                  </div>
-                  <div>
-                    <Button type="primary">ตกลง</Button>
-                  </div>
-                </div>
-              </CardHeader>
-
-            </Card>
+            </Tabs>       
           </TabPane>
         </Tabs>,
 
