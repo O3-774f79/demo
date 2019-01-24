1840f4845bcb45ad5e12746e37bea4b2c2eed96f
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2019-01-24 13:05:22 +0700

    init commit

---
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Badge,Nav, NavItem, NavLink,Button } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import logo from '../../assets/img/brand/logo.svg'
+import sygnet from '../../assets/img/brand/sygnet.svg'
+
+const propTypes = {
+  children: PropTypes.node,
+};
+
+const defaultProps = {};
+
+class DefaultHeader extends Component {
+  render() {
+
+    // eslint-disable-next-line
+    const { children, ...attributes } = this.props;
+
+    return (
+      <React.Fragment>
+        <AppSidebarToggler className="d-lg-none" display="md" mobile />
+        <AppNavbarBrand
+          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
+          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+        />
+        <AppSidebarToggler className="d-md-down-none" display="lg" />
+        <h1>System Name </h1>
+        <Nav className="ml-auto" navbar>
+          <NavItem className="d-md-down-none">
+            <NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
+          </NavItem>
+          <div style={{marginRight: 10,frontSize: 30}}>นาย สมชาย สามารถ</div>
+        </Nav>
+        <div><Button block color="light"className="btn-pill">ออกจากระบบ</Button></div>
+        {/* <AppAsideToggler className="d-md-down-none" /> */}
+        {/*<AppAsideToggler className="d-lg-none" mobile />*/}
+      </React.Fragment>
+    );
+  }
+}
+
+DefaultHeader.propTypes = propTypes;
+DefaultHeader.defaultProps = defaultProps;
+
+export default DefaultHeader;
---
+++ src/views/Product/index.js
@@ -0,0 +1,38 @@
+import React, { PureComponent } from 'react'
+import { Button, Card, CardBody, CardHeader, Col, Row } from 'reactstrap';
+
+export default class Index extends PureComponent {
+    render() {
+         return(
+            <div className="animated fadeIn">
+            <Card>
+              <CardHeader>
+                <strong><h4>Product</h4></strong>
+              </CardHeader>
+              <CardBody>
+                  
+            </CardBody>
+            </Card>
+            </div>  
+         )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
