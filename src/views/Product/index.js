ced9d901167f339df79fab0adfae9ad1a602fd28
Moo-ananchai <ananchai.pha@gmail.com>
2019-01-25 04:39:47 +0700

    edit icon

--- src/views/Product/index.js
+++ src/views/Product/index.js
@@ -1,13 +1,55 @@
 import React, { PureComponent } from 'react'
-import { Button, Card, CardBody, CardHeader, Col, Row } from 'reactstrap';
+import { Col, Row, Form, FormGroup, Label, Input, Card, CardBody, CardHeader,FormText } from 'reactstrap';
+import { Steps, Button, message,Icon,Tabs,Checkbox  } from 'antd';
+const Step = Steps.Step;
 
+const steps = [{
+  title: 'ฝ่ายจัดซื้อ',
+  status: 'finish',
+  icon: <Icon type="user" />
+}, {
+  title: 'ฝ่ายบัญชีและการเงิน',
+  status: 'process',
+  icon: <Icon type="loading" />
+}, {
+  title: 'เสร็จสิ้น',
+  status: 'wait',
+  icon: <Icon type="smile-o" />
+}];
+const stepsBuyer = [{
+  title: 'ซุปเปอร์ไวเซอร์',
+  status: 'finish',
+  icon: <Icon type="user" />
+}, {
+  title: 'ประธานกลุ่มสินค้า',
+  status: 'process',
+  icon: <Icon type="loading" />
+}, {
+  title: 'กรรมการผู้จัดการฝ่าย',
+  status: 'wait',
+  icon: <Icon type="user" />
+},
+{
+  title: 'ผู้จัดการฝ่าย',
+  status: 'wait',
+  icon: <Icon type="user" />
+}];
 export default class Index extends PureComponent {
+  state = {    current: 1,}
     render() {
+      const { current } = this.state;
          return(
             <div className="animated fadeIn">
+             <div><h2><b>ใบขอให้มี & แจ้งผลการทดสอบสินค้าตัวอย่าง</b></h2></div>
+            <hr></hr>
+                  <div style={{marginBottom: 10}}>
+        <Steps current={current}>
+                    {stepsBuyer.map(item => <Step key={item.title} title={item.title} status={item.status} icon={item.icon}/>)}
+                    </Steps>
+      </div>
             <Card>
               <CardHeader>
-                <strong><h4>Product</h4></strong>
+             
               </CardHeader>
               <CardBody>
                   
