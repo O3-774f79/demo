52b8610d06c93b4d765f59846af2b2fe514b53cb
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2019-01-25 15:44:29 +0700

    mockup data

---
+++ src/views/Inbox/EditableCell.js
@@ -0,0 +1,242 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import {
+  Table, Input, Button, Popconfirm, Form,
+} from 'antd';
+
+const FormItem = Form.Item;
+const EditableContext = React.createContext();
+
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends React.Component {
+  state = {
+    editing: false,
+  }
+
+  componentDidMount() {
+    if (this.props.editable) {
+      document.addEventListener('click', this.handleClickOutside, true);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.editable) {
+      document.removeEventListener('click', this.handleClickOutside, true);
+    }
+  }
+
+  toggleEdit = () => {
+    const editing = !this.state.editing;
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus();
+      }
+    });
+  }
+
+  handleClickOutside = (e) => {
+    const { editing } = this.state;
+    if (editing && this.cell !== e.target && !this.cell.contains(e.target)) {
+      this.save();
+    }
+  }
+
+  save = () => {
+    const { record, handleSave } = this.props;
+    this.form.validateFields((error, values) => {
+      if (error) {
+        return;
+      }
+      this.toggleEdit();
+      handleSave({ ...record, ...values });
+    });
+  }
+
+  render() {
+    const { editing } = this.state;
+    const {
+      editable,
+      dataIndex,
+      title,
+      record,
+      index,
+      handleSave,
+      ...restProps
+    } = this.props;
+    return (
+      <td ref={node => (this.cell = node)} {...restProps}>
+        {editable ? (
+          <EditableContext.Consumer>
+            {(form) => {
+              this.form = form;
+              return (
+                editing ? (
+                  <FormItem style={{ margin: 0 }}>
+                    {form.getFieldDecorator(dataIndex, {
+                      rules: [{
+                        required: true,
+                        message: `${title} is required.`,
+                      }],
+                      initialValue: record[dataIndex],
+                    })(
+                      <Input
+                        ref={node => (this.input = node)}
+                        onPressEnter={this.save}
+                      />
+                    )}
+                  </FormItem>
+                ) : (
+                  <div
+                    className="editable-cell-value-wrap"
+                    style={{ paddingRight: 24 }}
+                    onClick={this.toggleEdit}
+                  >
+                    {restProps.children}
+                  </div>
+                )
+              );
+            }}
+          </EditableContext.Consumer>
+        ) : restProps.children}
+      </td>
+    );
+  }
+}
+
+export default class EditableTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.columns = [
+    {
+      title: 'ประเภทเอกสาร',
+      dataIndex: 'ประเภทเอกสาร',
+      width: '13%',
+    }, {
+      title: 'เลขที่เอกสาร',
+      dataIndex: 'เลขที่เอกสาร',
+      width: '13%',
+    },{
+      title: 'รายละเอียด',
+      dataIndex: 'รายละเอียด',
+      width: '30%',
+    },
+    {
+        title: 'จาก',
+        dataIndex: 'จาก',
+        width: '10%',
+      },
+      {
+        title: 'วันที่รับเอกสาร',
+        dataIndex: 'วันที่รับเอกสาร',
+        width: '10%',
+      },
+     {
+      title: '',
+      dataIndex: 'operation',
+      render: (text, record) => (
+        this.state.dataSource.length >= 1
+          ? (
+           
+            <div>
+              <Button>แสดง</Button>
+              <Button>อนุมัติ</Button>
+              <Button>ไม่อนุมัติ</Button>
+              </div>
+   
+          ) : null
+      ),
+    }];
+
+    this.state = {
+      dataSource: [
+      { key: '1', ประเภทเอกสาร: 'เอกสาร PO',เลขที่เอกสาร:'ACT-FM070',รายละเอียด: 'ผลิตภัณฑ์ ชาวเกาะ',จาก:'นาย สมชาย กล้าหาร',วันที่รับเอกสาร:'25 ม.ค. 2562'},
+      { key: '2', ประเภทเอกสาร: 'เอกสาร ใบขอให้มี & แจ้งผลการทดสอบสินค้าตัวอย่าง',เลขที่เอกสาร:'PUR-FM003',รายละเอียด: 'ผลิตภัณฑ์ กะทิรอยไทย',จาก:'นาย สมสกุล กล้าหาร',วันที่รับเอกสาร:'30 ม.ค. 2562'},
+      { key: '3', ประเภทเอกสาร: 'เอกสาร NEW ITEM',เลขที่เอกสาร:' APF-FM027',รายละเอียด: 'ผลิตภัณฑ์ สารสกัดใบหม่อน',จาก:'นาง จริงใจ จริงจริง',วันที่รับเอกสาร:'2 ม.ค. 2562'},
+      { key: '4', ประเภทเอกสาร: 'เอกสาร ขอเพิ่ม SUPPLIER',เลขที่เอกสาร:'PUR-FM012',รายละเอียด: 'เจ้าของสวน มันสําปะหลัง จังหวัด ราชบุรี',จาก:'นาย สุขใจ พอมี',วันที่รับเอกสาร:'6 ธ.ค. 2561'},
+      { key: '5', ประเภทเอกสาร: 'เอกสาร PO',เลขที่เอกสาร:'ACT-FM080',รายละเอียด: 'ผลิตภัณฑ์ น้ำมันมะพร้าว รอยไทย',จาก:'นาย สามารถ ใจกล้า',วันที่รับเอกสาร:'25 ม.ค. 2562'},
+
+        
+      ],
+      count: 0,
+    };
+  }
+
+  handleDelete = (key) => {
+    const dataSource = [...this.state.dataSource];
+    this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
+  }
+
+  handleAdd = () => {
+    const { count, dataSource } = this.state;
+    const newData = {
+      key: count,
+      name: ``,
+      age: ``,
+      address: ``,
+    };
+    this.setState({
+      dataSource: [...dataSource, newData],
+      count: count + 1,
+    });
+  }
+
+  handleSave = (row) => {
+    const newData = [...this.state.dataSource];
+    const index = newData.findIndex(item => row.key === item.key);
+    const item = newData[index];
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+    this.setState({ dataSource: newData });
+  }
+
+  render() {
+    const { dataSource } = this.state;
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell,
+      },
+    };
+    const columns = this.columns.map((col) => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          editable: col.editable,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: this.handleSave,
+        }),
+      };
+    });
+    return (
+      <div>
+        <Table
+          components={components}
+          rowClassName={() => 'editable-row'}
+          bordered
+          title={() => 'Inbox'}
+          dataSource={dataSource}
+          columns={columns}
+        />
+      </div>
+    );
+  }
+}
+
+          
