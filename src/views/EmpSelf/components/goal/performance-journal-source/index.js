15b9349bd71c2ca41e41428e38ba971b80fb52fe
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2019-02-01 17:05:26 +0700

    add component emp dev

---
+++ src/views/EmpSelf/components/goal/performance-journal-source/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+import TableComponent from './TablePerformance'
+export default class index extends PureComponent {
+     render(){
+          return(
+               <div>
+                  <TableComponent />
+               </div>
+          )
+     }
+}
--- src/views/EmpSelf/index.js
+++ src/views/EmpSelf/index.js
@@ -1,11 +1,46 @@
 import React,{PureComponent} from 'react'
+import { Card } from 'antd';
+import 'antd/dist/antd.css';
+import  Goals from './components/goal'
+import Performances from './components/performance'
+import Review from './components/reviewForm'
+import Skill from './components/skill'
+const gridStyle = {
+    width: '25%',
+    textAlign: 'center',
+    cursor: "pointer",
+  };
 
 export default class  index extends PureComponent { 
+    state = {openConetent: 0}
+    onHandleClick = (t) =>{
+        this.setState({openConetent:t})
+    }
     render(){
+         const {openConetent} = this.state
         return(
-            <div>
-                EmpSelf
-            </div>
+            <React.Fragment>
+            <Card title="Employee Development" style={{width:"100%",backgroundColor:"#FFFF00"}}>
+            <Card.Grid style={gridStyle} onClick={()=>this.onHandleClick(1)}>Performance</Card.Grid>
+            <Card.Grid style={gridStyle} onClick={()=>this.onHandleClick(2)}>Goal</Card.Grid>
+            <Card.Grid style={gridStyle} onClick={()=>this.onHandleClick(3)}>Review</Card.Grid>
+            <Card.Grid style={gridStyle} onClick={()=>this.onHandleClick(4)}>Skill</Card.Grid>
+          </Card>
+            {(() => {
+                    switch(openConetent) {
+                    case 1:
+                        return <Performances />;
+                    case 2:
+                        return <Goals />;
+                    case 3:
+                        return <Review />
+                    case 4: 
+                        return <Skill />
+                    default:
+                        return null;
+                    }
+                })()}      
+          </React.Fragment>
         )
     }
 }
