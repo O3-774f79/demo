05a4c04645de7d9a46e325059f3812dd01b65adc
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2019-02-01 18:19:07 +0700

    add tab menu

--- src/views/EmpSelf/components/performance/index.js
+++ src/views/EmpSelf/components/performance/index.js
@@ -1,10 +1,19 @@
 import React, { PureComponent } from 'react'
- 
+import {Tabs} from 'antd'
+const TabPane = Tabs.TabPane
+
+const callback = (key)=>{
+    console.log(key)
+}
 export default class Index extends PureComponent { 
     render(){
          return(
               <div>
-                   Performance
+                <Tabs onChange={callback} type="card">
+                    <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+                    <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+                    <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+                </Tabs>,
               </div>
          )
     }
